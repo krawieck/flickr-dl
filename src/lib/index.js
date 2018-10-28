9f57d5e056c1dfd88a65a0d0cb9333108ff44aaa
krawieck <fil.krawczyk@gmail.com>
2018-10-28 10:21:16 +0100

    removed unused function

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,8 @@
 module.exports = {
   blockRequests: require('./blockRequests'),
-  dlPhoto: require('./dlPhoto'),
   getNumberOfPages: require('./getNumberOfPages'),
   getPhoto: require('./getPhoto'),
   getUrlsFromPage: require('./getUrlsFromPage'),
   getUrlsFromPages: require('./getUrlsFromPages'),
-  download: require('./download').download,
-  downloadAll: require('./download').downloadAll
+  download: require('./download').download
 }
