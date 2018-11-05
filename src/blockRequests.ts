db189772b1ff060b73fba4afe3e1e1f60ec5a3e1
krawieck <fil.krawczyk@gmail.com>
2018-11-05 20:53:16 +0100

    description now includes resource types

--- src/blockRequests.ts
+++ src/blockRequests.ts
@@ -3,6 +3,7 @@ import { Page, ResourceType } from 'puppeteer'
 /**
  * Blocks given types of resurces from getting downloaded. Should be called right before `page.goto()`
  *
+ * Resource types are: "document", "stylesheet", "image", "media", "font", "script", "texttrack", "xhr", "fetch", "eventsource", "websocket", "manifest" and "other"
  */
 export default function blockRequests(page: Page, ...resourceTypes: ResourceType[]): void {
   page.setRequestInterception(true)
@@ -14,16 +15,3 @@ export default function blockRequests(page: Page, ...resourceTypes: ResourceType
     }
   })
 }
-// | "document"
-// | "stylesheet"
-// | "image"
-// | "media"
-// | "font"
-// | "script"
-// | "texttrack"
-// | "xhr"
-// | "fetch"
-// | "eventsource"
-// | "websocket"
-// | "manifest"
-// | "other";
