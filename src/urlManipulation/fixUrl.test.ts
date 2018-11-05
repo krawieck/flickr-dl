424a67f14b6974bedf38441205c5a605be23356b
krawieck <fil.krawczyk@gmail.com>
2018-11-05 15:01:13 +0100

    more tests for fixUrl

--- src/urlManipulation/fixUrl.test.ts
+++ src/urlManipulation/fixUrl.test.ts
@@ -1,6 +1,6 @@
 import fixUrl from './fixUrl'
 
-test('fixes the darn urls', () => {
+it('fixes the darn urls', () => {
   const mockData = [
     [
       'https://www.flickr.com/photos/andreassofus/30677591587/in/explore-2018-10-29/',
@@ -17,6 +17,31 @@ test('fixes the darn urls', () => {
     [
       'https://www.flickr.com/photos/oldroger/27963939197/in/faves-57803084@N07/',
       'https://www.flickr.com/photos/oldroger/27963939197',
+    ],
+  ]
+
+  mockData.forEach(([input, output]) => {
+    expect(fixUrl(input)).toEqual(output)
+  })
+})
+
+it('accepts link without "www"', () => {
+  const mockData = [
+    [
+      'https://flickr.com/photos/andreassofus/30677591587/in/explore-2018-10-29/',
+      'https://flickr.com/photos/andreassofus/30677591587',
+    ],
+    [
+      'https://flickr.com/photos/megane_wakui/with/44607625822/',
+      'https://flickr.com/photos/megane_wakui',
+    ],
+    [
+      'https://flickr.com/photos/dtt67/45588983952/in/explore-2018-10-30/',
+      'https://flickr.com/photos/dtt67/45588983952',
+    ],
+    [
+      'https://flickr.com/photos/oldroger/27963939197/in/faves-57803084@N07/',
+      'https://flickr.com/photos/oldroger/27963939197',
     ],
   ]
 
